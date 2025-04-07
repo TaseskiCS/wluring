f1c185a531286aac24e49e7f0f8afbd9b462aa22
TaseskiCS <antoniotaseski.bus@hotmail.com>
2025-04-07 04:21:26 -0400

    refactor: moved form to FormPage, with only PR mode enabled

--- app/form/page.tsx
+++ app/form/page.tsx
@@ -1,12 +1,52 @@
+"use client"
+
 import React, {useState} from 'react'
+import { RingItem } from "../types/RingItem";
 
 const FormPage = () => {
+  const [ringItem, setRingItem] = useState<RingItem>({
+    username: "",
+    displayName: "",
+    url: "",
+    grad_date: "",
+  });
     
   const [showForm, setShowForm] = useState(false);
+  const [submitted, setSubmitted] = useState(false);
+  const [loading, setLoading] = useState(false);
   const [email, setEmail] = useState("");
+  const [error, setError] = useState<string | null>(null);
   const handleSubmit = async () => {
-    setShowForm(true);
-  }
+    if (!ringItem.displayName || !ringItem.url || !ringItem.grad_date) {
+      alert("Please fill in all fields!");
+      return;
+    }
+
+    setLoading(true);
+    setError(null);
+
+    try {
+      // Send the new data to the endpoint
+      const response = await fetch("/api/createPR", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(ringItem),
+      });
+
+      if (!response.ok) {
+        console.log(response);
+        throw new Error("Failed to submit the data.");
+      }
+      setSubmitted(true);
+    } catch (err) {
+      setError("An error occurred while submitting the ring item.");
+      console.error(err);
+    } finally {
+      setLoading(false);
+    }
+  };
   return (
     <div>
         {!showForm && (
@@ -25,14 +65,59 @@ const FormPage = () => {
         {showForm && (
     
             <>
-                <div className='mt-4'>
-                    <h1 className="text-3xl font-bold underline">Enter the OTP</h1>
-                    <input type="text" placeholder="Enter OTP" className="border-2 border-gray-300 rounded-md p-2 mt-4" />
+                <div className="flex flex-col items-center space-y-4">
+                    <input
+                        type="text"
+                        value={ringItem.username}
+                        onChange={(e) => setRingItem({ ...ringItem, username: e.target.value })}
+                        placeholder="Username"
+                        className="border-2 border-gray-300 rounded-md p-2 w-80"
+                    />
+                    <input
+                        type="text"
+                        value={ringItem.displayName}
+                        onChange={(e) => setRingItem({ ...ringItem, displayName: e.target.value })}
+                        placeholder="Name"
+                        className="border-2 border-gray-300 rounded-md p-2 w-80"
+                    />
+                    <input
+                        type="text"
+                        value={ringItem.url}
+                        onChange={(e) => setRingItem({ ...ringItem, url: e.target.value })}
+                        placeholder="URL"
+                        className="border-2 border-gray-300 rounded-md p-2 w-80"
+                    />
+                    <input
+                        type="text"
+                        value={ringItem.grad_date}
+                        onChange={(e) => setRingItem({ ...ringItem, grad_date: e.target.value })}
+                        placeholder="Graduation Date"
+                        className="border-2 border-gray-300 rounded-md p-2 w-80"
+                    />
+                    <button
+                        onClick={handleSubmit}
+                        className="bg-blue-500 text-white rounded-md p-2 w-80 hover:bg-blue-600 transition-all"
+                        disabled={loading}
+                        >
+                        {loading ? "Submitting..." : "Submit"}
+                    </button>
                 </div>
 
-                <button onClick={()=> setShowForm(false)}className='bg-blue-500 text-white rounded-md p-2 mt-4 hover:bg-blue-600 transition-all'>
-                    Go Back
-                </button>
+                {submitted && (
+                    <div className="mt-8 text-center">
+                        <h2 className="text-xl font-semibold">Successfully Submitted!</h2>
+                        <p>Username: {ringItem.username}</p>
+                        <p>Name: {ringItem.displayName}</p>
+                        <p>URL: {ringItem.url}</p>
+                        <p>Graduation Date: {ringItem.grad_date}</p>
+                    </div>
+                )}
+
+                {error && (
+                    <div className="mt-4 text-center text-red-500">
+                        <p>{error}</p>
+                    </div>
+                )}
             
             </>
         )}
--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,7 @@ import { RingItem } from "./types/RingItem";
 import FormPage from "./form/page";
 
 export default function Home() {
-  const [ringItem, setRingItem] = useState<RingItem>({
-    username: "",
-    displayName: "",
-    url: "",
-    grad_date: "",
-  });
-
-  const [submitted, setSubmitted] = useState(false);
   const [ringItems, setRingItems] = useState<RingItem[]>([]);
-  const [loading, setLoading] = useState(false);
   const [error, setError] = useState<string | null>(null);
 
   // fetch existing RingItems 
@@ -33,51 +24,6 @@ export default function Home() {
   }, []);
 
 
-  const handleSubmit = async () => {
-    if (!ringItem.displayName || !ringItem.url || !ringItem.grad_date) {
-      alert("Please fill in all fields!");
-      return;
-    }
-
-    setLoading(true);
-    setError(null);
-
-    try {
-      // Send the new data to the endpoint
-      const response = await fetch("/api/createPR", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify(ringItem),
-      });
-
-      if (!response.ok) {
-        console.log(response);
-        throw new Error("Failed to submit the data.");
-      }
-
-      // Add the new ring item to the list
-
-      setRingItems((prev) => [...prev, ringItem]); 
-
-      // Clear
-
-      setRingItem({
-        username: "",
-        displayName: "",
-        url: "",
-        grad_date: "",
-      });
-      setSubmitted(true);
-    } catch (err) {
-      setError("An error occurred while submitting the ring item.");
-      console.error(err);
-    } finally {
-      setLoading(false);
-    }
-  };
-
   return (
     <>
       <h1 className="flex justify-center h-screen items-center text-4xl font-bold">
@@ -89,61 +35,11 @@ export default function Home() {
         <pre>{JSON.stringify(ringItems, null, 2)}</pre>
       </div>
 
-      <FormPage/>
-
-      {/* <div className="flex flex-col items-center space-y-4">
-        <input
-            type="text"
-            value={ringItem.username}
-            onChange={(e) => setRingItem({ ...ringItem, username: e.target.value })}
-            placeholder="Username"
-            className="border-2 border-gray-300 rounded-md p-2 w-80"
-        />
-        <input
-          type="text"
-          value={ringItem.displayName}
-          onChange={(e) => setRingItem({ ...ringItem, displayName: e.target.value })}
-          placeholder="Name"
-          className="border-2 border-gray-300 rounded-md p-2 w-80"
-        />
-        <input
-          type="text"
-          value={ringItem.url}
-          onChange={(e) => setRingItem({ ...ringItem, url: e.target.value })}
-          placeholder="URL"
-          className="border-2 border-gray-300 rounded-md p-2 w-80"
-        />
-        <input
-          type="text"
-          value={ringItem.grad_date}
-          onChange={(e) => setRingItem({ ...ringItem, grad_date: e.target.value })}
-          placeholder="Graduation Date"
-          className="border-2 border-gray-300 rounded-md p-2 w-80"
-        />
-        <button
-          onClick={handleSubmit}
-          className="bg-blue-500 text-white rounded-md p-2 w-80 hover:bg-blue-600 transition-all"
-          disabled={loading}
-        >
-          {loading ? "Submitting..." : "Submit"}
+      <a href='/form'>
+        <button className="bg-blue-500 text-white rounded-md p-2 mt-4 hover:bg-blue-600 transition-all">
+          Add yourself to the ring!
         </button>
-      </div>
-
-      {submitted && (
-        <div className="mt-8 text-center">
-          <h2 className="text-xl font-semibold">Successfully Submitted!</h2>
-          <p>Username: {ringItem.username}</p>
-          <p>Name: {ringItem.displayName}</p>
-          <p>URL: {ringItem.url}</p>
-          <p>Graduation Date: {ringItem.grad_date}</p>
-        </div>
-      )}
-
-      {error && (
-        <div className="mt-4 text-center text-red-500">
-          <p>{error}</p>
-        </div>
-      )} */}
+      </a>
     </>
   );
 }
